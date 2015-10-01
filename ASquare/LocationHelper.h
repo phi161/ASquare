d80c20946525b3cad50dfe50e86d7782d04f4200
phi <phi@carminestudios.com>
2015-10-01 15:21:58 +0300

    Add documentation

--- ASquare/LocationHelper.h
+++ ASquare/LocationHelper.h
@@ -11,8 +11,16 @@
 
 typedef void (^LocationHelperCompletionBlock)(CLLocation *, NSError *);
 
+/**
+   This class is responsible for handling authorization of location services and returning the user's location. When reaching the desired accuracy, the location updated are stopped in order to save battery.
+   Creating an instance of the class does not instantiate `CLLocationManager`.
+ */
 @interface LocationHelper : NSObject
 
+/**
+   Starts location services and attempts to find the user's current location. If there is no authorization it will return an error, eitherwise it will call the completion block passing the location reported by `CLLocationManager`.
+   @param completionBlock A block that runs when there is a location update or if there is an authorization error
+ */
 -(void)findMyLocationWithSucces:(LocationHelperCompletionBlock)completionBlock;
 
 @end
