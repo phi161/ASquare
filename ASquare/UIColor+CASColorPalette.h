4178976d39e1d77f2eed8e5b6a8f09ec783f94cc
phi <phi@carminestudios.com>
2015-10-07 16:31:32 +0300

    Add a UIColor category to keep the colors used in the app in one place

---
+++ ASquare/UIColor+CASColorPalette.h
@@ -0,0 +1,23 @@
+//
+//  UIColor+CASColorPalette.h
+//  ASquare
+//
+//  Created by phi on 10/7/15.
+//  Copyright © 2015 Carmine Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/**
+   A helper class to keep colors consistent all over the app.
+   Names taken from http://chir.ag/projects/name-that-color
+ */
+@interface UIColor (CASColorPalette)
+
++(UIColor *)cas_contessaColor;
+
++(UIColor *)cas_tonysPinkColor;
+
++(UIColor *)cas_redRobinColor;
+
+@end
