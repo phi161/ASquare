61225584cc244e03fd8809e6de3fd1da43925dcf
phi <phi@carminestudios.com>
2015-10-07 03:36:07 +0300

    Add a UIImage category that overlays an image on top of another

---
+++ ASquare/UIImage+CASExtensions.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+CASExtensions.h
+//  ASquare
+//
+//  Created by phi on 10/7/15.
+//  Copyright © 2015 Carmine Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (CASExtensions)
+
+-(UIImage *)cas_imageWithOverlay:(UIImage *)overlay atPosition:(CGPoint)position;
+
+@end
